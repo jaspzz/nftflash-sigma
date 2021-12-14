3cb8b1f2e35305ffe5254db7d43897a339158e18
jaspzz <janspielvogel@gmail.com>
2021-12-14 23:34:18 +0100

    commit

--- getABI.js
+++ getABI.js
@@ -1,7 +1,5 @@
-const axios = require('axios')
-
 exports.handler = async function(request, response) {
-    const res = await axios.get(
+    const res = await axios_1.get(
         `https://api.etherscan.io/api?module=contract&action=getabi&address=${req.body.contractID}&apikey=` + process.env.INFURA_API_KEY
     )
     const abi = JSON.parse(res.data.result)
