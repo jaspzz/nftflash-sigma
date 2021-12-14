a77abfd4ff2e57fbc3a82d04b6ac5bf0a60c367c
jaspzz <janspielvogel@gmail.com>
2021-12-14 21:07:15 +0100

    Initial commit

---
+++ getABI.js
@@ -0,0 +1,9 @@
+exports.handler = function(request, response) {
+    const data = JSON.parse(request.body)
+    const res = await axios_1.get(
+        `https://api.etherscan.io/api?module=contract&action=getabi&address=${data.contractID}&apikey=` + process.env.INFURA_API_KEY
+    )
+    const abi = JSON.parse(res.data.result)
+    res.json({"abi":abi})
+    response.send({"message": "Successfully executed"});
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.nftflash = require("./nftflash.js").handler;
+exports.getABI = require("./getABI.js").handler;
