9b47df368ce98436f79f574ff6fd2395ec43a20c
jaspzz <janspielvogel@gmail.com>
2021-12-15 23:02:39 +0100

    Added CORS

--- getABI.js
+++ getABI.js
@@ -1,10 +1,13 @@
 const axios = require('axios')
 
 exports.handler = async function(request, response) {
+    res.set('Access-Control-Allow-Origin', "*")
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    const data = JSON.parse(request.body)
+    console.log(`https://api.etherscan.io/api?module=contract&action=getabi&address=${data.contractID}&apikey=` + process.env.INFURA_API_KEY)
     const res = await axios.get(
-        `https://api.etherscan.io/api?module=contract&action=getabi&address=${request.body.contractID}&apikey=` + process.env.INFURA_API_KEY
+        `https://api.etherscan.io/api?module=contract&action=getabi&address=${data.contractID}&apikey=` + process.env.INFURA_API_KEY
     )
-    functions.logger.log(`https://api.etherscan.io/api?module=contract&action=getabi&address=${request.body.contractID}&apikey=` + process.env.INFURA_API_KEY)
     const abi = JSON.parse(res.data.result)
     response.json({"abi":abi})
     response.send({"message": "Successfully executed"});
