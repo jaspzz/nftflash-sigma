5789760a06bdc4a562d9d33b1ba08cf51fc5646b
jaspzz <janspielvogel@gmail.com>
2021-12-15 01:01:32 +0100

    com

--- getABI.js
+++ getABI.js
@@ -2,9 +2,10 @@ const axios = require('axios')
 
 exports.handler = async function(request, response) {
     const res = await axios.get(
-        `https://api.etherscan.io/api?module=contract&action=getabi&address=${req.body.contractID}&apikey=` + process.env.INFURA_API_KEY
+        `https://api.etherscan.io/api?module=contract&action=getabi&address=${request.body.contractID}&apikey=` + process.env.INFURA_API_KEY
     )
+    functions.logger.log(`https://api.etherscan.io/api?module=contract&action=getabi&address=${request.body.contractID}&apikey=` + process.env.INFURA_API_KEY)
     const abi = JSON.parse(res.data.result)
-    res.json({"abi":abi})
+    response.json({"abi":abi})
     response.send({"message": "Successfully executed"});
 }
